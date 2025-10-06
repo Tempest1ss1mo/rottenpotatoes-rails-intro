46515e13c60a81fa420488c0801835ff03c045b1
Mingliang Yu <my2899@columbia.edu>
2025-10-06 09:16:46 +0000

    part 1 ver1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+  
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list.empty?
+      all
+    else
+      where('LOWER(rating) IN (?)', ratings_list.map(&:downcase))
+    end
+  end
 end
